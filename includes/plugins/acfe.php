060905c291d1bb224ad27a69ec4d40af28b970a9
jakob-connexxions <jakob@connexxions.me>
2024-12-11 10:07:50 +0000

    Update acfe.php
    Removed need of separate declared function to do simple task of disabling acfe modules

--- includes/plugins/acfe.php
+++ includes/plugins/acfe.php
@@ -2,22 +2,18 @@
 
 // ACF Extended - Disabling modules
 // =============================================================================
-if ( !function_exists( 'cx_acfe_modules' ) ) {
-    add_action('acfe/init', 'cx_acfe_modules');
-    function cx_acfe_modules(){
-        
-        // disable modules
-        acfe_update_setting('modules/author',           false);
-        acfe_update_setting('modules/categories',       false);
-        acfe_update_setting('modules/block_types',      false);
-        acfe_update_setting('modules/forms',            false);
-        acfe_update_setting('modules/options_pages',    false);
-        acfe_update_setting('modules/post_types',       false);
-        acfe_update_setting('modules/taxonomies',       false);
-        acfe_update_setting('modules/multilang',        false);
-        acfe_update_setting('modules/options',          false);
-        acfe_update_setting('modules/performance',      false);
-        acfe_update_setting('modules/ui',               false);
-        
-    }
-}
+
+add_action('acfe/init', function(){
+    // disable modules
+    acfe_update_setting('modules/author',           false);
+    acfe_update_setting('modules/categories',       false);
+    acfe_update_setting('modules/block_types',      false);
+    acfe_update_setting('modules/forms',            false);
+    acfe_update_setting('modules/options_pages',    false);
+    acfe_update_setting('modules/post_types',       false);
+    acfe_update_setting('modules/taxonomies',       false);
+    acfe_update_setting('modules/multilang',        false);
+    acfe_update_setting('modules/options',          false);
+    acfe_update_setting('modules/performance',      false);
+    acfe_update_setting('modules/ui',               false);
+});
