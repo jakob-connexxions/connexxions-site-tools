0ca7db438527c212ab672cc65024417a74646661
jakob-connexxions <jakob@connexxions.me>
2024-12-11 12:47:14 +0000

    Create options.php

---
+++ includes/admin/options.php
@@ -0,0 +1,14 @@
+<?php
+
+add_action( 'acf/init', function() {
+  	acf_add_options_page( array(
+    	'page_title' => 'Site Settings and Options — Connexxions',
+    	'menu_slug' => 'connexxions',
+    	'menu_title' => 'Connexxions',
+    	'position' => 1,
+        'capability' => 'edit_theme_options',
+    	'redirect' => false,
+    	'icon_url' => CXX_PLUGIN_URL , '/assets/cxx-dot.webp',
+    	'autoload' => true,
+    ) );
+} );
